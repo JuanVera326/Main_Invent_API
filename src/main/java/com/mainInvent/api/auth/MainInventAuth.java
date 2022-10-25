8848d2886ed3145834966f1d9b7bfa9929293bed
usuario <usuario@iMac-de-usuario.local>
2022-10-25 16:49:45 -0500

    Realizacion de querys de tipos

---
+++ src/main/java/com/mainInvent/api/auth/MainInventAuth.java
@@ -0,0 +1,66 @@
+package com.mainInvent.api.auth;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mainInvent.api.dto.AuthDto;
+import com.mainInvent.api.dto.UsuarioDto;
+import com.mainInvent.api.entity.UsuarioVo;
+import com.mainInvent.api.service.IUsuarioService;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = {"*"})
+public class MainInventAuth {
+	
+	@Autowired
+	private IUsuarioService authUser;
+	
+	@PostMapping("/auth")
+	public ResponseEntity<?> setAuth(@RequestBody AuthDto user){
+		
+		Optional<UsuarioVo> userAuth = authUser.encontrarPorCorreo(user.getEmail());
+		
+		try {
+			
+			if (userAuth.isEmpty()) {
+				
+				String msj = "Correo Icorrecto";
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(msj);
+				
+			}else {
+				
+				
+				if (user.getPassword().equals(userAuth.get().getPassword())) {
+					
+					UsuarioDto userAuthAccepted = new UsuarioDto( userAuth.get().getNombre(),
+							userAuth.get().getApellido(), userAuth.get().getCargo(), userAuth.get().getImagen(),
+							userAuth.get().getEdad(), userAuth.get().getRol(), userAuth.get().getCorreo(), 
+							userAuth.get().getEstado());
+					
+					return ResponseEntity.status(HttpStatus.ACCEPTED).body(userAuthAccepted);
+					
+				}
+				String msj = "Contraseña Incorrecta";
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(msj);
+			}
+			
+		} catch (Exception e) {
+			
+			String msj = "Denegado";
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(msj);
+			
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/mainInvent/api/dto/UsuarioDto.java
@@ -0,0 +1,102 @@
+package com.mainInvent.api.dto;
+
+public class UsuarioDto {
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private String cargo;
+	
+	private String imagen;
+	
+	private String edad;
+	
+	private int rol;
+	
+	private String correo;
+	
+	private boolean estado;
+	
+	
+	public UsuarioDto(){
+		
+	}
+	
+	public UsuarioDto(String nombre, String apellido, String cargo, String imagen, String edad, int rol, String correo, boolean estado){
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.cargo = cargo;
+		this.imagen = imagen;
+		this.edad = edad;
+		this.rol = rol;
+		this.correo = correo;
+		this.estado = estado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getEdad() {
+		return edad;
+	}
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+
+	public int getRol() {
+		return rol;
+	}
+
+	public void setRol(int rol) {
+		this.rol = rol;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+}
