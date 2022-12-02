04076743aa1bcd060b223248a325b5d8e786c8a4
usuario <usuario@iMac-de-usuario.local>
2022-12-02 16:14:54 -0500

    pruebas

--- src/main/java/com/mainInvent/api/controller/Documentos_DatasheetsRestController.java
+++ src/main/java/com/mainInvent/api/controller/Documentos_DatasheetsRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,7 +81,7 @@ public class Documentos_DatasheetsRestController {
 		return ResponseEntity.status(HttpStatus.ACCEPTED).body(file.get());
 	}
 	
-	@GetMapping("/files/{id}")
+	@DeleteMapping("/files/{id}")
 	public ResponseEntity<?> delFiles(@PathVariable String id){
 		
 		Optional<Documentos_DatasheetsVo> file = fileAct.findByID(id);
